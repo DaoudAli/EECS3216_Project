6e175eb51b3ab4163de91670b37d9d9a81540fd8
kingsley <king808@my.yorku.ca>
2023-04-01 22:47:13 -0400

    add LED rng

--- EECS3216_Project.v
+++ EECS3216_Project.v
@@ -16,6 +16,39 @@ parameter mole_timer = 32'd100000000; // 2s for mole to be on
 integer random = 0; // index of LED
 
 
+parameter time1s=32'd50000000;
+reg[35:0] time2s=32'd100000000;
+integer index1=8;
+
+// Internal signals
+reg [3:0] shift_reg;
+reg [3:0] random_number;
+initial shift_reg = 1;
+
+
+always @(posedge cin) begin
+counter <= counter+1;
+
+		if(counter >= time1s && index1>=-1) begin
+		   counter <= 32'b0;
+
+		   // LFSR for random number generation
+		   shift_reg <= {shift_reg[2:0], shift_reg[2] ^ shift_reg[1]};
+		 
+		   
+		   random_number = shift_reg;
+		   LEDR[random_number%9]=1;
+		   index1 = index1-1;
+			
+		end
+		
+		else if(index1<-1||KEY[0]==0)begin
+			LEDR<=8'b0;
+			index1=7;
+		end
+	end
+
+
 always @(posedge cin) begin
 	
 	LEDR[random] = 1;
